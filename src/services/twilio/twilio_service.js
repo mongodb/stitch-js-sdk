801e539864d3b90aa7f74249b1341725b4ba1259
Matt Broadstone <mbroadst@gmail.com>
2017-04-30 18:54:25 -0400

    feat(twilio-service): add new service definition for Twilio

---
+++ src/services/twilio/twilio_service.js
@@ -0,0 +1,33 @@
+/**
+ * Create a new TwilioService instance (not meant to be instantiated directly).
+ *
+ * @class
+ * @return {TwilioService} a TwilioService instance.
+ */
+class TwilioService {
+  constructor(baasClient, serviceName) {
+    this.baasClient = baasClient;
+    this.serviceName = serviceName;
+  }
+
+  /**
+   * Send a text message to a number
+   *
+   * @method
+   * @param {String} from number to send from
+   * @param {String} to number to send to
+   * @param {String} body SMS body content
+   * @return {Promise}
+   */
+  send(from, to, body) {
+    return self.db.client.executePipeline([
+      {
+        service: this.serviceName,
+        action: 'send',
+        args: { from, to, body }
+      }
+    ]);
+  }
+}
+
+export default TwilioService;
