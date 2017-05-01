8c670d88994f737d21bf1cdde12f40080f063e37
Matt Broadstone <mbroadst@gmail.com>
2017-05-01 13:24:42 -0400

    feat(s3-service): add s3 service

---
+++ src/services/aws/s3_service.js
@@ -0,0 +1,54 @@
+/**
+ * Convenience wrapper around AWS S3 service (not meant to be instantiated directly).
+ *
+ * @class
+ * @return {S3Service} a S3Service instance.
+ */
+class S3Service {
+  constructor(baasClient, serviceName) {
+    this.client = baasClient;
+    this.serviceName = serviceName;
+  }
+
+  /**
+   * Put an object to S3 via BaaS. For small uploads
+   *
+   * NOTE: body must be a pipeline stream
+   *
+   * @param {String} bucket which S3 bucket to use
+   * @param {String} key which key (filename) to use
+   * @param {String} acl which policy to apply
+   * @param {String} contentType content type of uploaded data
+   * @return {Promise}
+   */
+  put(bucket, key, acl, contentType) {
+    return this.client.executePipeline([
+      {
+        service: this.serviceName,
+        action: 'put',
+        args: { bucket, key, acl, contentType }
+      }
+    ]);
+  }
+
+  /**
+   * Sign a policy for putting via BaaS. For large uploads
+   *
+   * @param {String} bucket which S3 bucket to use
+   * @param {String} key which key (filename) to use
+   * @param {String} acl which policy to apply
+   * @param {String} contentType content type of uploaded data
+   * @return {Promise}
+   */
+  signPolicy(bucket, key, acl, contentType) {
+    return this.client.executePipeline([
+      {
+        service: this.serviceName,
+        action: 'signPolicy',
+        args: { bucket, key, acl, contentType }
+      }
+    ]);
+  }
+}
+
+export default S3Service;
