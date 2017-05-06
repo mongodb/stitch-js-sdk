564fa0656b2492f02af4ac2baaa73c37fac26a85
Eric Daniels <eric@erdaniels.com>
2017-05-06 17:44:14 -0400

    feat(groups): BAAS-136 - use master.baas-dev.10gen.cc as base url

--- test/test.js
+++ test/test.js
@@ -7,12 +7,12 @@ import { mocks } from 'mock-browser';
 
 const EJSON = require('mongodb-extjson');
 
-const ANON_AUTH_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/auth/anon/user';
-const APIKEY_AUTH_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/auth/api/key';
-const LOCALAUTH_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/auth/local/userpass';
-const PIPELINE_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/pipeline';
-const NEW_ACCESSTOKEN_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/auth/newAccessToken';
-const BASEAUTH_URL = 'https://baas-master.10gen.cc/api/client/v1.0/app/testapp/auth';
+const ANON_AUTH_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/auth/anon/user';
+const APIKEY_AUTH_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/auth/api/key';
+const LOCALAUTH_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/auth/local/userpass';
+const PIPELINE_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/pipeline';
+const NEW_ACCESSTOKEN_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/auth/newAccessToken';
+const BASEAUTH_URL = 'https://master.baas-dev.10gen.cc/api/client/v1.0/app/testapp/auth';
 const ejson = new EJSON();
 
 const MockBrowser = mocks.MockBrowser;
