9d44c996c9a66bfe54d71ff7ee421c65b3797a2f
Jay Flax <jsflax@gmail.com>
2018-06-05 13:35:16 -0400

    added missing test

--- packages/core-services-twilio/src/__tests__/CoreTwilioServiceClientUnitTests.ts
+++ packages/core-services-twilio/src/__tests__/CoreTwilioServiceClientUnitTests.ts
@@ -37,37 +37,39 @@ describe("CoreTwilioServiceClientUnitTests", () => {
     expect(client.sendMessage(to, from, body)).rejects.toThrow();
   });
 
-  // @Test
-  // public void testSendMessageWithMedia() {
-  //   final CoreStitchServiceClient service = Mockito.mock(CoreStitchServiceClient.class);
-  //   final CoreTwilioServiceClient client = new CoreTwilioServiceClient(service);
+  it("should send message with media", async () => {
+    const serviceMock = mock(CoreStitchServiceClientImpl);
+    const service = instance(serviceMock);
 
-  //   final String to = "+15558509552";
-  //   final String from = "+15558675309";
-  //   final String body = "I've got it!";
-  //   final String mediaUrl = "https://jpegs.com/myjpeg.gif.png";
+    const to = "+15558509552";
+    const from = "+15558675309";
+    const body = "I've got your number";
+    const mediaUrl = "https://jpegs.com/myjpeg.gif.png";
 
-  //   client.sendMessage(to, from, body, mediaUrl);
+    const client = new CoreTwilioServiceClient(service);
 
-  //   final ArgumentCaptor<String> funcNameArg = ArgumentCaptor.forClass(String.class);
-  //   final ArgumentCaptor<List> funcArgsArg = ArgumentCaptor.forClass(List.class);
-  //   verify(service).callFunctionInternal(funcNameArg.capture(), funcArgsArg.capture());
+    await client.sendMessage(to, from, body, mediaUrl);
 
-  //   assertEquals("send", funcNameArg.getValue());
-  //   assertEquals(1, funcArgsArg.getValue().size());
-  //   final Document expectedArgs = new Document();
-  //   expectedArgs.put("to", to);
-  //   expectedArgs.put("from", from);
-  //   expectedArgs.put("body", body);
-  //   expectedArgs.put("mediaUrl", mediaUrl);
-  //   assertEquals(expectedArgs, funcArgsArg.getValue().get(0));
+    const [funcName, funcArgsArg] = capture(
+      serviceMock.callFunctionInternal
+    ).last();
 
-  //   // Should pass along errors
-  //   doThrow(new IllegalArgumentException("whoops"))
-  //       .when(service).callFunctionInternal(any(), any());
-  //   assertThrows(() -> {
-  //     client.sendMessage(to, from, body);
-  //     return null;
-  //   }, IllegalArgumentException.class);
-  // }
+    expect(funcName).toEqual("send");
+    expect((funcArgsArg as any[]).length).toBe(1);
+    const expectedArgs = {
+      body,
+      from,
+      mediaUrl,
+      to
+    };
+
+    expect(funcArgsArg[0]).toEqual(expectedArgs);
+
+    // Should pass along errors
+    when(serviceMock.callFunctionInternal(anything(), anything())).thenReject(
+      new Error("whoops")
+    );
+
+    expect(client.sendMessage(to, from, body)).rejects.toThrow();
+  });
 });
