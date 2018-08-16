805a65e0adf0be0cf470e04d6e5461ec9cfe01f9
Adam Chelminski <adam.chelminski@mongodb.com>
2018-08-16 18:46:04 -0400

    properly publish docs to major and minor version paths

--- contrib/publish_docs.sh
+++ contrib/publish_docs.sh
@@ -6,8 +6,8 @@ VERSION=`node -e 'console.log(require("../lerna.json").version)'`
 VERSION_MAJOR=$(echo $VERSION | cut -d. -f1)
 VERSION_MINOR=$(echo $VERSION | cut -d. -f2)
 VERSION_PATCH=$(echo $VERSION | cut -d. -f3 | cut -d- -f1)
-VERSION_QUALIFIER=$(echo $VERSION | cut -d- -f2)
-VERSION_QUALIFIER_INC=$(echo $VERSION | cut -d- -f3)
+VERSION_QUALIFIER=$(echo $VERSION | cut -d- -f2 -s)
+VERSION_QUALIFIER_INC=$(echo $VERSION | cut -d- -f3 -s)
 
 npm run docs
 
