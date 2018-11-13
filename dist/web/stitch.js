b0caa9d04485b4e771726b71f16ab5a5b66e2ebb
Eric Daniels <eric@erdaniels.com>
2018-11-13 16:16:29 -0500

    Release 3.2.11

--- dist/web/stitch.js
+++ dist/web/stitch.js
@@ -197,7 +197,7 @@ var DEFAULT_STITCH_SERVER_URL = exports.DEFAULT_STITCH_SERVER_URL = 'https://sti
 // VERSION is substituted with the package.json version number at build time
 var version = 'unknown';
 if (true) {
-  version = "3.2.10";
+  version = "3.2.11";
 }
 var SDK_VERSION = exports.SDK_VERSION = version;
 
