e9a34dce46c7a71cb1858312c8960e5432c223c7
Kwuang Tang <kwuang.tang@mongodb.com>
2019-08-07 20:08:19 -0400

    Move related registration tests

--- test/userRegistrations.test.js
+++ test/userRegistrations.test.js
@@ -1,5 +1,5 @@
 import StitchMongoFixture from './fixtures/stitch_mongo_fixture';
-
+import { PROVIDER_TYPE_USERPASS } from '../src/auth/providers';
 import { buildClientTestHarness, extractTestFixtureDataPoints } from './testutil';
 
 
@@ -11,12 +11,14 @@ describe('User Registrations', ()=>{
   beforeAll(() => test.setup());
   afterAll(() => test.teardown());
 
-  const testEmail = 'link_user@10gen.com';
+  const linkEmail = 'link_user@10gen.com';
+  const testEmail = 'test_user@10gen.com';
   const password = '123456';
 
   beforeEach(async() => {
     const { apiKey, groupId, serverUrl } = extractTestFixtureDataPoints(test);
     th = await buildClientTestHarness(apiKey, groupId, serverUrl);
+    await th.configureAnon();
     client = th.stitchClient;
     await client.logout();
   });
@@ -42,5 +44,46 @@ describe('User Registrations', ()=>{
     await th.app().userRegistrations().removePendingUserByID(userID);
     expect(await findPendingUsersByID(userID)).toHaveLength(0);
     expect(await getPendingUsers()).toHaveLength(1);
+  });
+  it('should link a user that was confirmed via `confirmByEmail` with associated provider', async() => {
+    const userId = await client.login();
+    const { identities } = await client.userProfile();
+    expect(identities.length).toEqual(1);
+
+    await client.register(linkEmail, password);
+
+    await th.app().userRegistrations().confirmByEmail(linkEmail);
+    const newUserId = await client.linkWithProvider('userpass', { username: linkEmail, password });
+    expect(userId).toEqual(newUserId);
+
+    const { identities: newIdentities } = await client.userProfile();
+    expect(newIdentities.length).toEqual(2);
+
+    expect(newIdentities[0].provider_type).toEqual('anon-user');
+    expect(newIdentities[1].provider_type).toEqual('local-userpass');
+  });
+  it('should list pending users', async() => {
+    await client.register(testEmail, password);
+    await client.login();
+    await client.register(linkEmail, password);
+
+    let pendingUsers = await th.app().userRegistrations().listPending({limit: '5'});
+    expect(pendingUsers).toHaveLength(3);
+
+    pendingUsers = await th.app().userRegistrations().listPending({limit: '1'});
+    expect(pendingUsers).toHaveLength(1);
+
+    await th.app().userRegistrations().confirmByEmail(linkEmail);
+    await client.linkWithProvider('userpass', { username: linkEmail, password });
+
+    pendingUsers = await th.app().userRegistrations().listPending({limit: '3'});
+    expect(pendingUsers).toHaveLength(2);
+
+    await th.app().userRegistrations().confirmByEmail(testEmail);
+    await client.login(testEmail, password);
+    expect(client.auth.loggedInProviderType).toEqual(PROVIDER_TYPE_USERPASS);
+
+    pendingUsers = await th.app().userRegistrations().listPending({limit: '3'});
+    expect(pendingUsers).toHaveLength(1);
   });
 });
