e2a3242147d1effaa1decdeab282a5acee70e266
Jason Flax <jsflax@gmail.com>
2020-01-03 15:00:57 +0000

    Separate tests, lint

--- packages/browser/coretest/__tests__/StitchCustomUserDataIntTests.ts
+++ packages/browser/coretest/__tests__/StitchCustomUserDataIntTests.ts
@@ -1,6 +1,20 @@
 import { BaseStitchBrowserIntTestHarness } from "mongodb-stitch-browser-testutils";
-import { AnonProviderConfig, MongoConfig, MongoDbRule, MongoDbService, Role, Schema, ServiceResource, Rule, UserpassProviderConfig, UserpassProvider, CustomUserConfigData, StitchFunction } from "mongodb-stitch-core-admin-client";
+import { 
+	AnonProviderConfig, 
+	CustomUserConfigData, 
+	MongoConfig,
+	MongoDbRule,
+	MongoDbService,
+	Role,
+	Rule,
+	Schema,
+	ServiceResource,
+	StitchFunction,
+	UserpassProvider,
+	UserpassProviderConfig
+} from "mongodb-stitch-core-admin-client";
 import { UserPasswordCredential } from "mongodb-stitch-core-sdk";
+import { StitchAppClient } from "mongodb-stitch-browser-core";
 
 const harness = new BaseStitchBrowserIntTestHarness();
 
@@ -16,6 +30,8 @@ describe("StitchAppClient", () => {
 	const colorField = "favoriteColor";
 	const favoriteColor = "blue";
 
+	let client: StitchAppClient;
+
 	it("should define custom data", async () => {
 		const { app: appResponse, appResource: app } = await harness.createApp();
 		await harness.addProvider(app, new AnonProviderConfig());
@@ -27,8 +43,7 @@ describe("StitchAppClient", () => {
 			db,
 			coll,
 			[new Role()],
-			new Schema()
-		));
+			new Schema()));
 
 		await harness.addProvider(app,
 			new UserpassProvider(new UserpassProviderConfig(
@@ -42,8 +57,7 @@ describe("StitchAppClient", () => {
 			db,
 			coll,
 			"recoome",
-			true
-		));
+			true));
 
 		await app.functions.create(new StitchFunction(
 			"addUserProfile",
@@ -61,7 +75,7 @@ describe("StitchAppClient", () => {
 			`,
 			undefined));
 
-		const client = harness.getAppClient(appResponse);
+		client = harness.getAppClient(appResponse);
 		await harness.registerAndLoginWithUserPass(app, client, user, password);
 		expect(client.auth.user).toBeDefined();
 		expect(client.auth.user!!.customData).toEqual({});
@@ -71,11 +85,13 @@ describe("StitchAppClient", () => {
 		await client.auth.refreshCustomData();
 
 		expect(client.auth.user!!.customData[colorField]).toEqual(favoriteColor);
+	});
 
+	it("should have custom data defined on login", async () => {
 		await client.auth.logout();
 
 		await client.auth.loginWithCredential(new UserPasswordCredential(user, password));
 
 		expect(client.auth.user!!.customData[colorField]).toEqual(favoriteColor);
-	});
+	})
 });
