7122164aeb2be5f467a93a064a57364990f51dcd
Marcus Todd <malexander.todd@gmail.com>
2020-01-27 18:05:41 -0500

    STITCH-4381: Add schema generation support to admin sdk (#356)

--- test/admin/realm.test.js
+++ test/admin/realm.test.js
@@ -1,12 +1,13 @@
 const StitchMongoFixture = require('../fixtures/stitch_mongo_fixture');
 
-import { buildAdminTestHarness,
-  extractTestFixtureDataPoints } from '../testutil';
+import { buildAdminTestHarness, extractTestFixtureDataPoints, createSampleMongodbService } from '../testutil';
 
 describe('Realm', () => {
   let test = new StitchMongoFixture();
   let th;
   let realmConfig;
+  let realmClientSchemas;
+  let services;
 
   beforeAll(() => test.setup());
   afterAll(() => test.teardown());
@@ -15,6 +16,8 @@ describe('Realm', () => {
     const { apiKey, groupId, serverUrl } = extractTestFixtureDataPoints(test);
     th = await buildAdminTestHarness(true, apiKey, groupId, serverUrl);
     realmConfig = th.app().realm().config();
+    realmClientSchemas = th.app().realm().clientSchemas();
+    services = th.app().services();
   });
 
   afterEach(async() => th.cleanup());
@@ -35,6 +38,40 @@ describe('Realm', () => {
       await realmConfig.update(newConfig);
       const updatedConfig = await realmConfig.get();
       expect(updatedConfig).toEqual(newConfig);
+    });
+  });
+
+  describe('clientSchemas', () => {
+    describe('.get()', async() => {
+      it('should return a 400 error if called with an unsupported language', async() => {
+        try {
+          await realmClientSchemas.get('PERL');
+        } catch (e) {
+          expect(e.response.status).toEqual(400);
+          expect(e.message).toEqual("Unsupported client language: 'PERL'");
+        }
+      });
+
+      it('should return a 500 error if called when no MongoDB service exists', async() => {
+        try {
+          await realmClientSchemas.get('KOTLIN');
+        } catch (e) {
+          expect(e.response.status).toEqual(500);
+          expect(e.message).toEqual('error processing request');
+        }
+      });
+
+      it('should return an error if called when the app has no MongoDB service rules', async() => {
+        // Setup MongoDB service
+        await createSampleMongodbService(services);
+
+        try {
+          await realmClientSchemas.get('KOTLIN');
+        } catch (e) {
+          expect(e.response.status).toEqual(500);
+          expect(e.message).toEqual('error processing request');
+        }
+      });
     });
   });
 });
