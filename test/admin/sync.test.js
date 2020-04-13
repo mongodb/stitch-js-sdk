35a49ccbbe2b8a75e891db7e5795756ab9f8287d
Nick Makes <nick@makes.life>
2020-04-13 11:54:40 -0400

    REALMC-5162: Add sync data admin api endpoint to admin client (#383)

--- test/admin/sync.test.js
+++ test/admin/sync.test.js
@@ -1,10 +1,17 @@
 const StitchMongoFixture = require('../fixtures/stitch_mongo_fixture');
 
-import { buildAdminTestHarness, extractTestFixtureDataPoints, createSampleMongodbService, addRuleToMongodbService } from '../testutil';
+import {
+  buildAdminTestHarness,
+  extractTestFixtureDataPoints,
+  createSampleMongodbService,
+  createSampleMongodbSyncService,
+  addRuleToMongodbService
+} from '../testutil';
 
 describe('Sync', () => {
   let test = new StitchMongoFixture();
   let th;
+  let sync;
   let syncConfig;
   let syncClientSchemas;
   let services;
@@ -15,8 +22,9 @@ describe('Sync', () => {
   beforeEach(async() => {
     const { apiKey, groupId, serverUrl } = extractTestFixtureDataPoints(test);
     th = await buildAdminTestHarness(true, apiKey, groupId, serverUrl);
-    syncConfig = th.app().sync().config();
-    syncClientSchemas = th.app().sync().clientSchemas();
+    sync = th.app().sync();
+    syncConfig = sync.config();
+    syncClientSchemas = sync.clientSchemas();
     services = th.app().services();
   });
 
@@ -76,10 +84,39 @@ describe('Sync', () => {
       it('should succed with a supported language and and rules', async() => {
         // Setup MongoDB service
         const mongoDBService = await createSampleMongodbService(services);
-        await addRuleToMongodbService(services, mongoDBService, {database: 'foo', collection: 'bar'});
-        const schemas =  await syncClientSchemas.get('KOTLIN');
+        await addRuleToMongodbService(services, mongoDBService, { database: 'foo', collection: 'bar' });
+        const schemas = await syncClientSchemas.get('KOTLIN');
         expect(schemas.length).toEqual(1);
       });
+    });
+  });
+
+  describe('data', () => {
+    it('should return nothing if called when the app has no MongoDB sync service', async() => {
+      const data = await sync.data();
+      expect(data).toEqual({});
+    });
+
+    it('should return correct data for an enabled sync service', async() => {
+      const syncService = await createSampleMongodbSyncService(services, 'email');
+      await addRuleToMongodbService(services, syncService, {
+        database: 'db',
+        collection: 'coll',
+        config: {
+          schema: {
+            properties: {
+              _id: { bsonType: 'objectId' },
+              email: { bsonType: 'string' },
+              active: { bsonType: 'boolean' },
+              store_id: { bsonType: 'objectId' },
+              created_at: { bsonType: 'long' }
+            }
+          }
+        }
+      });
+
+      const data = await sync.data();
+      expect(data).toEqual({ service_id: syncService._id, partition_fields: ['created_at', 'email', 'store_id'] });
     });
   });
 });
