89367ed3314ff27069dbc8586c4f2b0352fd1226
Gabri <gabriele.cimato@gmail.com>
2020-05-07 10:28:04 -0500

    test(createCluster): add unit tests for cluster creation request args

--- test/recaptcha.test.js
+++ test/recaptcha.test.js
@@ -4,14 +4,14 @@ import { StitchAdminClientFactory } from '../src/admin';
 describe('Recaptcha', () => {
   const token = 'my-little-token';
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     global['fetch'] = fetch;
     fetch.mockResponseOnce(Promise.resolve());
     let adminClient = await StitchAdminClientFactory.create();
     await adminClient.verifyRecaptcha(token);
   });
 
-  afterEach(() => fetch.resetMocks());
+  afterAll(() => fetch.resetMocks());
 
   it('should be verified with cors enabled', () => {
     expect(fetch.mock.calls.length).toEqual(1);
@@ -31,5 +31,10 @@ describe('Recaptcha', () => {
   it('should let the request determine the Content Type', () => {
     expect(fetch.mock.calls.length).toEqual(1);
     expect(fetch.mock.calls[0][1].headers['Content-Type']).toBeUndefined();
+  });
+
+  it('should not require Stitch specific header', () => {
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][1].headers['X-STITCH-Request-Origin']).toBeUndefined();
   });
 });
