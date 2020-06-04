6b7f57e3ff1d5909a1fc12cfadd7dd9b1d5e1557
Eric Daniels <eric@erdaniels.com>
2020-06-04 11:01:34 -0400

    Release 3.18.0 (#424)

--- dist/node/common.js
+++ dist/node/common.js
@@ -14,8 +14,8 @@ var DEFAULT_STITCH_SERVER_URL = exports.DEFAULT_STITCH_SERVER_URL = 'https://rea
 
 // VERSION is substituted with the package.json version number at build time
 var version = 'unknown';
-if (typeof "3.17.1" !== 'undefined') {
-  version = "3.17.1";
+if (typeof "3.18.0" !== 'undefined') {
+  version = "3.18.0";
 }
 var SDK_VERSION = exports.SDK_VERSION = version;
 
